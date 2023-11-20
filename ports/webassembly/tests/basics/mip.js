8a4396ad5b55412983c9d14214ffef0e67f55c14
Jeff Glass <glass.jeffrey@gmail.com>
2023-11-19 18:33:40 -0600

    Add TODO for tests

--- ports/webassembly/tests/basics/mip.js
+++ ports/webassembly/tests/basics/mip.js
@@ -7,3 +7,21 @@ mp.loadMicroPython({url:"../../build/micropython.wasm"}).then(py=>{
     py.runPython("print(keyword.kwlist")
 });
 });
+
+/*
+Tests to write once tests are running successfully:
+mip.install("pkgname")  # Installs the latest version of "pkgname" (and dependencies)
+mip.install("pkgname", version="x.y")  # Installs version x.y of "pkgname"
+mip.install("pkgname", mpy=False)  # Installs the source version (i.e. .py rather than .mpy files)
+mip.install("pkgname", target="third-party")
+sys.path.append("third-party")
+mip.install("http://example.com/x/y/foo.py")
+mip.install("http://example.com/x/y/foo.mpy")
+mip.install("github:org/repo/path/foo.py")  # Uses default branch
+mip.install("github:org/repo/path/foo.py", version="branch-or-tag")  # Optionally specify the branch or tag
+mip.install("http://example.com/x/package.json")
+mip.install("github:org/user/path/package.json")
+mip.install("http://example.com/x/")
+mip.install("github:org/repo")  # Uses default branch of that repo
+mip.install("github:org/repo", version="branch-or-tag")
+*/
